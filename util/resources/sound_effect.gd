324017764079f160231f10914d4088073be43785
Charpurrr <66517745+Charpurrr@users.noreply.github.com>
2026-03-12 01:48:42 +0100

    New SoundEffect Resource
    Adds a replacement for the previous SFX.play_sfx() function. This one allows for easier and grander customisability (and can be added onto without interfering with pre-existing calls unlike the previous system.)
    
    For more information, view the SoundEffect documentation.

---
+++ util/resources/sound_effect.gd
@@ -0,0 +1,191 @@
+@tool
+class_name SoundEffect
+extends Resource
+## A singular sound effect played using a temporary [AudioStreamPlayer] or [AudioStreamPlayer2D]. [br]
+##
+## A sound effect gets assigned to 2 groups, one representing all SFX played by 
+## the same audio bus, and one for all SFX played at [method play]'s [code]from[/code] node.
+## The names of these groups are [code]<bus name>[/code] and
+## [code]<node name>/sfx[/code] respectively.
+
+## What the sound effect sounds like.
+var stream: AudioStream
+## What audio bus the sound effect gets played in.
+var audio_bus: StringName = &"Master"
+## At what volume the sound effect plays.
+## Note that this is an addition to the audio bus' volume and not an overwrite.
+var volume: float = 0.0
+## At what pitch the sound effect plays.
+var pitch: float = 1.0
+
+## Whether or not the sound effect is 2 dimensional.
+## (Played at a position in the world.)
+var is_2d: bool = true:
+	set(val):
+		is_2d = val
+		notify_property_list_changed()
+## Whether or not the position at which the sound effect plays
+## is the same as [method play]'s [code]from[/code] node's position.
+var inherit_position: bool = true:
+	set(val):
+		inherit_position = val
+		notify_property_list_changed()
+## What global position the sound effect plays at
+## if [member inherit_position] is [code]false[/code].
+var position := Vector2.ZERO
+## The volume is attenuated over distance with this as an exponent.
+var attenuation: float = 1.0
+## Maximum distance from which audio is still hearable.
+var max_distance: float = 2000.0
+
+
+## Creates an [AudioStreamPlayer] or [AudioStreamPlayer2D] depending on [member is_2d],
+## adds it to [param from], then destroys it when the sound effect finishes.[br]
+## Returns an optionally usable reference to the assigned [AudioStreamPlayer] or [AudioStreamPlayer2D].
+func play(from: Node) -> Variant:
+	if not is_instance_valid(from):
+		push_error("Cannot fetch node %s." % from.name)
+		return
+
+	if stream == null:
+		push_error("SoundEffect has no AudioStream value.")
+		return
+
+	var player = null
+
+	if is_2d:
+		player = AudioStreamPlayer2D.new()
+
+		player.attenuation = attenuation
+		player.max_distance = max_distance
+
+		if not inherit_position:
+			player.position = position
+	else:
+		player = AudioStreamPlayer.new()
+
+	player.set_stream(stream)
+	player.set_bus(audio_bus)
+	# For referencing all sfx in the bus.
+	player.add_to_group(audio_bus)
+	# For referencing all sfx in the parent. (For example, all sfx from a [State].)
+	player.add_to_group(from.name + "/sfx")
+	player.set_volume_db(volume)
+	player.set_pitch_scale(pitch)
+
+	from.add_child(player)
+
+	player.connect(&"finished", player.queue_free)
+	player.play()
+
+	return player
+
+
+func _get_property_list() -> Array[Dictionary]:
+	var audio_buses: PackedStringArray
+
+	for i in range(AudioServer.bus_count):
+		audio_buses.append(AudioServer.get_bus_name(i))
+
+	var properties: Array[Dictionary] = [
+		{
+			"name": "stream",
+			"type": TYPE_OBJECT,
+			"hint": PROPERTY_HINT_RESOURCE_TYPE,
+			"hint_string": "AudioStream",
+			"usage": PROPERTY_USAGE_DEFAULT,
+		},
+		{
+			"name": "audio_bus",
+			"type": TYPE_STRING,
+			"hint": PROPERTY_HINT_ENUM,
+			"hint_string": ",".join(audio_buses),
+			"usage": PROPERTY_USAGE_DEFAULT,
+		},
+		{
+			"name": "volume",
+			"type": TYPE_FLOAT,
+			"hint": PROPERTY_HINT_RANGE,
+			"hint_string": "0,0,0.005,or_greater,or_less,hide_control,suffix:dB",
+			"usage": PROPERTY_USAGE_DEFAULT,
+		},
+		{
+			"name": "pitch",
+			"type": TYPE_FLOAT,
+			"usage": PROPERTY_USAGE_DEFAULT,
+		},
+		{
+			"name": "2 Dimensional",
+			"type": TYPE_NIL,
+			"usage": PROPERTY_USAGE_GROUP
+		},
+		{
+			"name": "is_2d",
+			"type": TYPE_BOOL,
+			"hint": PROPERTY_HINT_GROUP_ENABLE,
+			#"hint_string": "checkbox_only",
+			"usage": PROPERTY_USAGE_DEFAULT,
+		}
+	]
+
+	if is_2d:
+		properties.append_array([
+			{
+				"name": "attenuation",
+				"type": TYPE_FLOAT,
+				"usage": PROPERTY_USAGE_DEFAULT,
+			},
+			{
+				"name": "max_distance",
+				"type": TYPE_FLOAT,
+				"usage": PROPERTY_USAGE_DEFAULT,
+			},
+			{
+				"name": "inherit_position",
+				"type": TYPE_BOOL,
+				"usage": PROPERTY_USAGE_DEFAULT,
+			}
+		])
+
+	if is_2d and not inherit_position:
+		properties.append(
+			{
+				"name": "position",
+				"type": TYPE_VECTOR2,
+				"hint": PROPERTY_HINT_LINK,
+				"usage": PROPERTY_USAGE_DEFAULT,
+			}
+		)
+
+	return properties
+
+
+func _property_can_revert(property: StringName) -> bool:
+	match property:
+		&"stream", &"audio_bus", &"play_at", &"volume", &"pitch", &"is_2d", \
+		&"attenuation", &"max_distance", &"inherit_position", &"position":
+			return true
+
+	return false
+
+
+func _property_get_revert(property: StringName) -> Variant:
+	match property:
+		&"stream":
+			return null
+		&"audio_bus":
+			return &"Master"
+		&"play_at":
+			return ""
+		&"volume":
+			return 0.0
+		&"pitch", &"attenuation":
+			return 1.0
+		&"max_distance":
+			return 2000.0
+		&"is_2d", &"inherit_position":
+			return true
+		&"position":
+			return Vector2.ZERO
+
+	return null
