44f91e28dee9dbe8217964b25145a6af327826df
Davin <146634489+davin-christino@users.noreply.github.com>
2026-03-23 17:35:21 +0530

    ButtSlide Rewrite
    finally finished The Thing, trust me it's Fantastic. sorry for Dragging it for so long, this rewrite was Massive, but unlike the Low Taper this will never Fade.
    
    covered all bases (i think) except for 1. the animations, the offset variables aren't actually used, looks like. not experienced with the animation system in solar engine so i didn't bother touching it.
    2. the weird bug when you fall off slopes when sliding at high speeds, although this happens in other states too so the bug might be a fundamental flaw with mario, not with this state. removing the first line in _on_enter makes it worse, this might be something to look into.

--- entities/player/shared/states/dry/butt_slide.gd
+++ entities/player/shared/states/dry/butt_slide.gd
@@ -2,30 +2,11 @@ class_name ButtSlide
 extends PlayerState
 ## Crouching while on a slope (or being forced.)
 
-## Default sliding speed.
-@export var speed: float = 10
-## Minimum sliding speed (through decelerating by holding the opposite direction you're sliding in.)
-@export var min_speed: float = 5
-## Maximum sliding speed (through accelerating by holding the direction you're sliding in.)
-@export var max_speed: float = 20
-## Minimum sliding speed necessary to remain in a buttslide state.
-## (In practice: how much you'll be sliding on flat ground after sliding off a slope.)
-@export var min_remain_speed: float = 0.1
-
-## Friction applied when sliding along flat or shallow ground.
-@export var friction: float = 0.125
-
-## Maximum acceleration from sliding, when a slope is as steep as possible.
-@export var max_accel: float = 0.22
-
-## How much speed is lost when changing slope angle.
-## Higher turn forgiveness means more speed is lost.
-@export_exp_easing() var turn_forgiveness: float = 2.0
-
-## Higher shallowness penalty means shallow slopes will decrease speed more.
-@export_exp_easing() var shallow_penalty: float = 0.5
-## Higher friction means less speed conservation on steep slopes.
-@export_exp_easing() var incline_friction: float = 10.0
+@export var friction_coefficient_default: float
+@export var friction_coefficient_decel: float
+@export var accel_gravity_multiplier: float
+@export var max_speed: float
+@export var min_remain_speed: float
 
 @export_category(&"Animation (Unique to State)")
 @export var animation_forward: StringName
@@ -35,39 +16,29 @@ extends PlayerState
 @export var animation_airborne: StringName
 @export var anim_offset_a: Vector2
 
-## Speed at which the player slides.
-var slide_speed: float = 0.0
-
-## Stored previous direction, for changing slope angle.
-var old_direction: Vector2
-
-## True when the player does not have a slide direction yet.
-var no_direction: bool
-
-var input_dir: int
-
 
 func _on_enter(handover_speed):
+	# TODO: find out why this line is here. removing it causes some weird behaviour but idk why
 	actor.set_floor_snap_length(movement.snap_length)
-
+	
+	# Convert vertical speed from ground pounding into sliding speed
 	if handover_speed is float:
-		old_direction = Vector2.DOWN
-		slide_speed = handover_speed
-	else:
-		no_direction = true
-		slide_speed = 0
+		actor.velocity = (Vector2.DOWN * handover_speed).slide(actor.get_floor_normal())
 
 
 func _physics_tick(delta: float):
+	# Apply physics
 	if actor.is_on_floor():
 		_grounded(delta)
 	else:
 		_airborne(delta)
-
+	
+	# Set animations
 	_set_appropriate_anim()
 
 
 func _subsequent_ticks(_delta: float):
+	# Emit particles and play sound effects
 	if actor.is_on_floor():
 		particles[0].emit_at(actor)
 
@@ -79,168 +50,70 @@ func _subsequent_ticks(_delta: float):
 
 ## Buttsliding on the ground / slope.
 func _grounded(delta: float):
-	var floor_normal: Vector2 = actor.get_floor_normal()
-
-	var slide_direction: Vector2
-
-	slide_direction = _get_slide_dir(floor_normal)
-	input_dir = InputManager.get_x_dir()
-
-	if no_direction:
-		_apply_slide_direction(floor_normal, slide_direction)
-	else:
-		# If we're changing direction, redirect current sliding speed into the new direction
-		if not slide_direction.is_equal_approx(old_direction):
-			_redirect_slide(slide_direction, old_direction)
-
-	# Modify slide_speed prior to setting velocity
-	_modify_speed(floor_normal)
-
-	actor.velocity = slide_direction * slide_speed
-	actor.velocity.y += 6.0 * delta
-
-	# Store the sliding direction so we can check for changes
-	old_direction = slide_direction
+	# Equal to 1 if accelerating, -1 if decelerating, 0 otherwise.
+	var accel_dir: int = InputManager.get_x_dir() * sign(actor.velocity.x)
+	# Coefficient of friction, should be increased when decelerating.
+	var friction: float = friction_coefficient_decel if accel_dir == -1 else friction_coefficient_default
+	# Acceleration due to sliding from gravity, should be increased when accelerating.
+	var slide_accel: float = movement.max_grav
+	if accel_dir == 1: slide_accel *= accel_gravity_multiplier
+	
+	# Apply gravity
+	actor.velocity += Vector2.DOWN.slide(actor.get_floor_normal()) * slide_accel * delta
+	
+	# Calculate normal acceleration
+	var normal_accel = Vector2.UP.dot(actor.get_floor_normal()) * slide_accel
+	
+	# Calculate frictional acceleration
+	actor.velocity = actor.velocity.move_toward(Vector2.ZERO, friction * normal_accel * delta)
+	
+	# Cap the speed
+	actor.velocity = actor.velocity.limit_length(max_speed)
 
 
 ## Buttsliding in the air.
 func _airborne(delta: float):
-	no_direction = true
-	slide_speed = 0
-
 	movement.apply_gravity(delta)
-
-
-## Get the slide direction downwards along the slope.
-func _get_slide_dir(floor_normal: Vector2) -> Vector2:
-	var direction: Vector2
-
-	if floor_normal == Vector2.UP:
-		# On flat ground, just go right.
-		direction = Vector2.RIGHT
-	else:
-		direction = floor_normal.orthogonal()
-
-		if direction.dot(Vector2.DOWN) < 0:
-			direction = -direction
-
-	return direction
-
-
-## Integrate current velocity and direction to a slide.
-func _apply_slide_direction(floor_normal: Vector2, slide_direction: Vector2):
-	# If we have only just started sliding,
-	# we need to convert the player's velocity into sliding speed.
-	if floor_normal == Vector2.UP:
-		slide_speed = actor.velocity.x
-	else:
-		if abs(actor.velocity.x) > abs(actor.velocity.dot(slide_direction)):
-			slide_speed = actor.velocity.x * sign(floor_normal.x)
-		else:
-			slide_speed = actor.velocity.dot(slide_direction)
-
-	no_direction = false
-
-
-## Calculates acceleration, deceleration, and target speed; then applies them.
-func _modify_speed(floor_normal: Vector2):
-	# How flat the floor is, 0-1
-	var flatness: float = floor_normal.dot(Vector2.UP)
-	# How steep the floor is (opposite of flatness, just for convenience)
-	var steepness: float = 1 - flatness
-	var slope_dir: int = sign(floor_normal.x)
-
-	var accel_ease: float = ease(steepness, shallow_penalty)
-
-	var slide_accel: float = accel_ease * max_accel
-	var slide_decel: float = ease(flatness, incline_friction) * friction
-
-	var target_speed: float = accel_ease * speed
-
-	if input_dir != 0:
-		# Holding the direction of the slope (accel)
-		if input_dir == slope_dir:
-			target_speed = accel_ease * max_speed
-		# Holding the opposite direction of the slope (decel)
-		elif input_dir == -slope_dir:
-			target_speed = accel_ease * min_speed
-		
-	slide_decel = max(slide_decel, 6.0)
-
-	# Accelerate down the slope
-	_accelerate(slide_accel, target_speed)
-	# Decelerate due to friction
-	_decelerate(slide_decel, target_speed)
-
-
-func _redirect_slide(new: Vector2, old: Vector2):
-	# Calculate how similar the new direction is to the old direction
-	var preserved = new.dot(old)
-
-	# Store the sign: -1 if we have moved from an upward slope to a downward slope
-	# or vice versa
-	var new_sign = sign(preserved)
-
-	# Take absolute value so we can work with 0 to 1 instead of -1 to 1
-	preserved = abs(preserved)
-
-	# Calculate how much speed would be lost
-	var lost = 1 - preserved
-	# Apply forgiveness to what was lost
-	var lost_forgiven = ease(lost, turn_forgiveness)
-
-	# Subtract the reduced loss from 1
-	var final_preserved = 1 - lost_forgiven
-
-	slide_speed *= final_preserved * new_sign
-
-
-func _accelerate(amount: Variant, speed_cap: float):
-	if slide_speed + amount < speed_cap:
-		slide_speed += amount
-	elif slide_speed < speed_cap:
-		slide_speed = speed_cap
-
-
-func _decelerate(amount: float, target: float):
-	var direction = sign(slide_speed)
-	var abs_speed = abs(slide_speed)
-
-	if abs_speed > target:
-		abs_speed = max(abs_speed - amount, target)
-
-	slide_speed = abs_speed * direction
 
 
 ## Set the animation based on how you're moving on a slope.
 func _set_appropriate_anim():
+	# Play airborne animation if not on floor
 	if not actor.is_on_floor():
 		actor.doll.play(animation_airborne)
 		return
-	if input_dir != 0:
+	
+	# Play moving forward/backward if left/right is held.
+	if InputManager.get_x_dir() != 0:
 		actor.doll.play(
-			animation_forward if input_dir == movement.facing_direction else animation_backward
+			animation_forward if InputManager.get_x_dir() == movement.facing_direction else animation_backward
 		)
 		return
+	
+	# Play default animation otherwise.
 	actor.doll.play(animation_data.animation)
 
 
 func _trans_rules():
+	# When slow enough on flat ground, exit the sliding state.
 	if not movement.is_slide_slope() and abs(actor.velocity.x) < min_remain_speed:
-		return [&"Crouch", [true, false]]
+		if Input.is_action_pressed(&"down"):
+			return [&"Crouch", [true, false]]
+		else:
+			return &"Idle"
 
-	if not Input.is_action_pressed(&"down"):
+	# Exit the sliding state when up is pressed.
+	if input.buffered_input(&"up"):
 		if actor.is_on_floor():
-			if old_direction.y == 0 and input_dir != 0 or Input.is_action_just_pressed(&"up"):
-				return &"Idle"
-			if input_dir == -sign(old_direction.x):
-				return &"Walk"
+			return &"Idle"
 		else:
 			return &"Fall"
 
+	# Jump out of a buttslide:
 	if actor.is_on_floor() and input.buffered_input(&"jump"):
 		return &"ButtSlideJump"
 
+	# Spin out of a buttslide:
 	if input.buffered_input(&"spin"):
 		return &"Spin"
 
